e31f09a3917afadcc263e77f94cd78f33d8ffbb8
Sarah Basinger <sarrey@gmail.com>
2016-12-13 09:13:08 -0500

    form now functional. working on additional styling

--- validate.js
+++ validate.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		event.preventDefault();
 
 			$('input').each(function(){
-				var currentTagClass = $(this).attr('class');
+				var currentTagClass = $(this).attr('id');
 				var errorClass = '.' + currentTagClass + '-error';
 				if($(this).val() == ''){
 					$(errorClass).html('This field is required');
@@ -14,29 +14,29 @@ $(document).ready(function(){
 				}
 			})
 
-			var password = $('.password').val();
-			var password2 = $('.password2').val();
+			var password = $('#password').val();
+			var password2 = $('#password2').val();
 			var numberFound = false;
 			for(let i = 0; i<password.length; i++){
 				if(isNaN(Number(password[i]))){
-					// there is not a number
+					// there is a number
 				}else{
 					numberFound = true;
 				}
 			}
 
 			if(password !== password2){
-				$('password-error').html("Your passwords do not match")
-				$('password-error').show();
+				$('.password-error').html("Your passwords do not match")
+				$('.password-error').show();
 			}else if(password.length<6){
-				$('password-error').html("Your password must be at least 6 characters.")
-				$('password-error').show();
+				$('.password-error').html("Your password must be at least 6 characters.")
+				$('.password-error').show();
 			}else if(!numberFound){
-				$('password-error').html("Your password is weak. Try adding a number.")
-				$('password-error').addClass(".caution")
-				$('password-error').show();
+				$('.password-error').html("Your password is weak. Try adding a number.")
+				$('.password-error').addClass(".caution")
+				$('.password-error').show();
 			}else{
-				$('password-error').hide();
+				$('.password-error').hide();
 			}
 	});
 });
